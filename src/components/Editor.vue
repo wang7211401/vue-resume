01c8fa7cd624fd9226fd5c333a354005a0d10fe9
wang7211401 <wang7211401@163.com>
2017-09-25 16:42:42 +0800

    重构个人信息

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -11,23 +11,76 @@
       </ol>
     </nav>
     <ol class="panes">
-      <li :class="{active: currentType === 0}">tab1</li>
-      <li :class="{active: currentType === 1}">tab2</li>
-      <li :class="{active: currentType === 2}">tab3</li>
-      <li :class="{active: currentType === 3}">tab4</li>
-      <li :class="{active: currentType === 4}">tab5</li>
-      <li :class="{active: currentType === 5}">tab6</li>
+      <li :class="{active: currentType === 0}">
+        <ProfileEditor :profile="profile"></ProfileEditor>
+      </li>
+      <li :class="{active: currentType === 1}">
+        <h2>工作经历</h2>
+        <el-form>
+          <div class="work-container" v-for="(work,index) in this.workHistory">
+            <el-form-item label="公司">
+              <el-input v-model="work.company"></el-input>
+            </el-form-item>
+            <el-form-item label="工作内容">
+              <el-input v-model="work.content"></el-input>
+            </el-form-item>
+            <i class="el-icon-circle-close" @click="removeWorkHistory(index)"></i>
+            <hr>
+          </div>
+          <el-button type="primary" @click="addWorkHistory">添加工作经历</el-button>
+        </el-form>
+      </li>
+      <li :class="{active: currentType === 2}">
+        <h2>学习经历</h2>
+      </li>
+      <li :class="{active: currentType === 3}">
+        <h2>项目经历</h2>
+      </li>
+      <li :class="{active: currentType === 4}">
+        <h2>获奖经历</h2>
+      </li>
+      <li :class="{active: currentType === 5}">
+        <h2>联系方式</h2>
+      </li>
     </ol>
   </div>
 </template>
 <script>
+  import ProfileEditor from './ProfileEditor'
   export default {
+      components:{ProfileEditor},
       data(){
           return {
             currentType:0,
-            icons:['shenfenzheng','work','book','c-project-copy','jiangbei','phone-line']
+            icons:['shenfenzheng','work','book','c-project-copy','jiangbei','phone-line'],
+            profile:{
+                name:'',
+                city:'',
+                birth:''
+            },
+            workHistory:[
+              {company:'',content:''}
+            ]
           }
+      },
+      methods:{
+          addWorkHistory(){
+              this.workHistory.push({
+                  company:'',
+                  content:''
+              })
+          },
+          removeWorkHistory(index){
+              this.workHistory.splice(index,1)
+          }
+      },
+      created(){
+        console.log(this.profile)
+        setTimeout(()=>{
+            console.log(this.profile)
+        },1000)
       }
+
   }
 </script>
 <style>
@@ -61,11 +114,24 @@
     width: 100%;
     height: 100%;
     background: #fff;
+    overflow:auto;
   }
   .panes li{
     display: none;
+    padding:32px;
   }
   .panes li.active{
     display:block;
+  }
+  .panes li h2{
+    margin-top:20px;
+  }
+  .work-container{
+    position:relative;
+  }
+  .el-icon-circle-close{
+    position:absolute;
+    top:0;
+    right:0;
   }
 </style>
